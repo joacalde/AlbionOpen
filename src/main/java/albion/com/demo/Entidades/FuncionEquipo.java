f230f4784e7b688ba3935d5e22ebee581053c917
joacalde <joacalde3@gmail.com>
2023-09-23 11:40:31 -0300

    arreglo error falta de @Entity

--- src/main/java/albion/com/demo/Entidades/FuncionEquipo.java
+++ src/main/java/albion/com/demo/Entidades/FuncionEquipo.java
@@ -2,12 +2,14 @@ package albion.com.demo.Entidades;
 
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import org.hibernate.annotations.GenericGenerator;
 
+@Entity
 public class FuncionEquipo {
     
     @Id
--- src/main/java/albion/com/demo/Repositorios/FuncionEquipoRepositorio.java
+++ src/main/java/albion/com/demo/Repositorios/FuncionEquipoRepositorio.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface FuncionEquipoRepositorio   extends JpaRepository<FuncionEquipo, String>{
+public interface FuncionEquipoRepositorio extends JpaRepository<FuncionEquipo, String>{
     
     @Query("SELECT a FROM FuncionEquipo a WHERE a.posicion = :posicion")
     public FuncionEquipo posicion(@Param("posicion") int posicion);
