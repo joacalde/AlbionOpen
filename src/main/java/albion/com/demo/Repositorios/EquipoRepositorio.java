94c078dd8315ac96df98067b85fc283ae02b576f
joacalde <joacalde3@gmail.com>
2023-09-23 11:26:41 -0300

    sacar respositorio antiguo Equipo

--- src/main/java/albion/com/demo/Repositorios/EquipoRepositorio.java
+++ src/main/java/albion/com/demo/Repositorios/EquipoRepositorio.java
@@ -20,28 +20,4 @@ public interface EquipoRepositorio  extends JpaRepository<Equipo, String> {
     @Query("SELECT max(a.posicion) FROM Linea a")
     public Integer ultimaPosicion();
     
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto1 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto1Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto2 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto2Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto3 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto3Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto4 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto4Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto5 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto5Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto6 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto6Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto7 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto7Id(@Param("fotoId") String fotoId);
-
-    @Query("SELECT e FROM Equipo e LEFT JOIN e.foto8 f WHERE f.id = :fotoId")
-    public Equipo equipoPorFoto8Id(@Param("fotoId") String fotoId);
-    
 }
